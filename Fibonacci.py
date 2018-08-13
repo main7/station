011a6472328af9fe029b388e7e126371df1aca1b
click <465053823@qq.com>
2018-08-13 16:36:53 +0800

    git push

---
+++ Fibonacci.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+#def fib(max):
+#   n,a,b=0,0,1
+#    while n<max:
+#        print(b)
+#        a,b=b,a+b
+#        n=n+1
+#    return 'done'
+
+
+def fib(max):
+    n,a,b=0,0,1
+    while n<max:
+        yield b
+        a,b=b,a+b
+        n=n+1
+    return 'done'
